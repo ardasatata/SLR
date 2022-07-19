8412dcf73d874a4d7dd0465b983ecdc1e1ac3b31
minelab-ubuntu <a@a.com>
2022-07-19 19:22:24 +0800

    remove BiLSTM - dgx

--- slr_network_mf_new.py
+++ slr_network_mf_new.py
@@ -98,10 +98,7 @@ class SLRModelMF(nn.Module):
             return torch.cat([tensor, tensor.new(length - tensor.size(0), *tensor.size()[1:]).zero_()])
 
         x = torch.cat([inputs[len_x[0] * idx:len_x[0] * idx + lgt] for idx, lgt in enumerate(len_x)])
-        # print(x.shape)
         x = self.conv2d(x)
-        # print(x.shape)
-        # print('masked_bn')
         x = torch.cat([pad(x[sum(len_x[:idx]):sum(len_x[:idx + 1])], len_x[0])
                        for idx, lgt in enumerate(len_x)])
         return x
@@ -111,18 +108,12 @@ class SLRModelMF(nn.Module):
             return torch.cat([tensor, tensor.new(length - tensor.size(0), *tensor.size()[1:]).zero_()])
 
         x = torch.cat([inputs[len_x[0] * idx:len_x[0] * idx + lgt] for idx, lgt in enumerate(len_x)])
-        # print(x.shape)
         x = self.conv2d_1ch(x)
-        # print(x.shape)
-        # print('masked_bn')
         x = torch.cat([pad(x[sum(len_x[:idx]):sum(len_x[:idx + 1])], len_x[0])
                        for idx, lgt in enumerate(len_x)])
         return x
 
     def forward(self, x, key_x, len_x, label=None, label_lgt=None):
-        # print(x.shape)
-        # print(len_x.shape)
-        # print(key_x.shape)
 
         batch, temp, placeholder, point, axis = key_x.shape
         inputs_kp = key_x.reshape(batch * temp, placeholder, point, axis)
@@ -158,33 +149,21 @@ class SLRModelMF(nn.Module):
             x = self.temporal_attn(block_1, block_1, block_1)
             x_key = self.temporal_attn_key(block_1_key, block_1_key, block_1_key)
 
-            # cross_attn = self.temporal_attn(block_1_key, block_1, block_1)
-            # x = self.temporal_attn(block_1, block_1, block_1)
-            # x_key = self.temporal_attn(block_1_key, block_1_key, block_1_key)
-
             lgt = conv1d_outputs['feat_len']
         else:
             conv1d_outputs = self.conv1d(framewise, len_x)
-            # conv1d_outputs_key = self.conv1d(keypoints, len_x)
-            # x: T, B, C
+            conv1d_outputs_key = self.conv1d_key(keypoints, len_x)
+
             x = conv1d_outputs['visual_feat']
-            # x_key: T, B, C
-            # x_key = conv1d_outputs_key['visual_feat']
+            x_key = conv1d_outputs_key['visual_feat']
+
             # feature length
             lgt = conv1d_outputs['feat_len']
 
         # concat
         x_cat = torch.cat([x, x_key], 2)
 
-        # tm_outputs = self.temporal_model(x, lgt)
-        tm_outputs = self.temporal_model(x_cat, lgt)
-
-        # tm_outputs_ff = self.temporal_model(x, lgt)
-        # tm_outputs_key = self.temporal_model(x_key, lgt)
-
-        outputs = self.classifier(tm_outputs['predictions'])
-        # outputs_ff = self.classifier(tm_outputs_ff['predictions'])
-        # outputs_key = self.classifier(tm_outputs_key['predictions'])
+        outputs = x_cat
 
         pred = None if self.training \
             else self.decoder.decode(outputs, lgt, batch_first=False, probs=False)
@@ -248,115 +227,6 @@ class SLRModelMF(nn.Module):
         self.loss['CTCLoss'] = torch.nn.CTCLoss(reduction='none', zero_infinity=False)
         self.loss['distillation'] = SeqKD(T=8)
         return self.loss
-
-
-# def scaled_dot_product(q, k, v, mask=None):
-#     d_k = q.size()[-1]
-#     attn_logits = torch.matmul(q, k.transpose(-2, -1))
-#     attn_logits = attn_logits / math.sqrt(d_k)
-#     if mask is not None:
-#         attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
-#     attention = F.softmax(attn_logits, dim=-1)
-#     values = torch.matmul(attention, v)
-#     return values, attention
-#
-#
-# class MultiheadAttention(nn.Module):
-#
-#     def __init__(self, input_dim, embed_dim, num_heads):
-#         super().__init__()
-#         assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
-#
-#         self.embed_dim = embed_dim
-#         self.num_heads = num_heads
-#         self.head_dim = embed_dim // num_heads
-#
-#         # Stack all weight matrices 1...h together for efficiency
-#         # Note that in many implementations you see "bias=False" which is optional
-#         self.qkv_proj = nn.Linear(input_dim, 3 * embed_dim)
-#         self.o_proj = nn.Linear(embed_dim, embed_dim)
-#
-#         self._reset_parameters()
-#
-#     def _reset_parameters(self):
-#         # Original Transformer initialization, see PyTorch documentation
-#         nn.init.xavier_uniform_(self.qkv_proj.weight)
-#         self.qkv_proj.bias.data.fill_(0)
-#         nn.init.xavier_uniform_(self.o_proj.weight)
-#         self.o_proj.bias.data.fill_(0)
-#
-#     def forward(self, x, mask=None, return_attention=False):
-#         batch_size, seq_length, embed_dim = x.size()
-#         qkv = self.qkv_proj(x)
-#
-#         # Separate Q, K, V from linear output
-#         qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3 * self.head_dim)
-#         qkv = qkv.permute(0, 2, 1, 3)  # [Batch, Head, SeqLen, Dims]
-#         q, k, v = qkv.chunk(3, dim=-1)
-#
-#         # Determine value outputs
-#         values, attention = scaled_dot_product(q, k, v, mask=mask)
-#         values = values.permute(0, 2, 1, 3)  # [Batch, SeqLen, Head, Dims]
-#         values = values.reshape(batch_size, seq_length, embed_dim)
-#         o = self.o_proj(values)
-#
-#         if return_attention:
-#             return o, attention
-#         else:
-#             return o
-
-# def attention(q, k, v, d_k, mask=None, dropout=None):
-#     scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
-#     if mask is not None:
-#         mask = mask.unsqueeze(1)
-#         scores = scores.masked_fill(mask == 0, -1e9)
-#     scores = F.softmax(scores, dim=-1)
-#
-#     if dropout is not None:
-#         scores = dropout(scores)
-#
-#     output = torch.matmul(scores, v)
-#     return output
-#
-#
-# class MultiHeadAttention(nn.Module):
-#     def __init__(self, heads, d_model, dropout=0.1):
-#         super().__init__()
-#
-#         self.d_model = d_model
-#         self.d_k = d_model // heads
-#         self.h = heads
-#
-#         self.q_linear = nn.Linear(d_model, d_model)
-#         self.v_linear = nn.Linear(d_model, d_model)
-#         self.k_linear = nn.Linear(d_model, d_model)
-#         self.dropout = nn.Dropout(dropout)
-#         self.out = nn.Linear(d_model, d_model)
-#
-#     def forward(self, q, k, v, mask=None):
-#         bs = q.size(0)
-#
-#         # perform linear operation and split into h heads
-#
-#         k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
-#         q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
-#         v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
-#
-#         # transpose to get dimensions bs * h * sl * d_model
-#
-#         k = k.transpose(1, 2)
-#         q = q.transpose(1, 2)
-#         v = v.transpose(1, 2)
-#         # calculate attention using function we will define next
-#         scores = attention(q, k, v, self.d_k, mask, self.dropout)
-#
-#         # concatenate heads and put through final linear layer
-#         concat = scores.transpose(1, 2).contiguous() \
-#             .view(bs, -1, self.d_model)
-#
-#         output = self.out(concat)
-#
-#         return output
 
 # A helper function for producing N identical layers
 def clones(module, N):
